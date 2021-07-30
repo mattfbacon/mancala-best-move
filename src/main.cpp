c053d2cfbbe7630c0f15a0f7a62921d66c1d02ef
Matt Fellenz <mattf53190@gmail.com>
2021-07-30 07:52:16 -0700

    Use a vector instead of a list for max path
    Since the item of the container is a single byte (MancalaBin aka signed char), storing a pointer to the next element is a huge overhead. Switching to vector led to noticeable performance improvements.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <list>
 #include <memory>
+#include <stack>
 #include <utility>
+#include <vector>
 
 #include "MancalaTree.hpp"
 
-// returns the moves in REVERSE ORDER
-std::pair<std::list<MancalaBin>, signed char> tree_max(MancalaTree* root, std::list<MancalaBin> const& current_path = {}) {
+// pair ( path to max, max )
+std::pair<std::vector<MancalaBin>, signed char> tree_max(MancalaTree* root, std::vector<MancalaBin> const& current_path = {}) {
 	// Base case
 	if (root == nullptr) {
 		return { current_path, std::numeric_limits<signed char>::min() };
@@ -19,7 +20,7 @@ std::pair<std::list<MancalaBin>, signed char> tree_max(MancalaTree* root, std::l
 		if (auto const& item = root->items[i]; item.get() != nullptr) {
 			std::lock_guard<std::mutex> max_guard{ max_mutex };
 			auto their_path = current_path;  // copy
-			their_path.emplace_front(static_cast<MancalaBin>(i));
+			their_path.emplace_back(static_cast<MancalaBin>(i));
 			if (auto const new_max = tree_max(item.get(), their_path); new_max.second > current_max.second) {
 				current_max = new_max;
 			}
@@ -30,18 +31,17 @@ std::pair<std::list<MancalaBin>, signed char> tree_max(MancalaTree* root, std::l
 
 void print_max(signed char num_in_each) {
 	std::unique_ptr<MancalaTree> const tree{ MancalaTree::make(num_in_each) };
-	auto const max = tree_max(tree.get());
-	auto const end_iter = std::crend(max.first);
+	auto const [path_to_max, max] = tree_max(tree.get());
 	MancalaBoard board{ num_in_each };
 	bool invalid = false;
-	for (auto iter = std::crbegin(max.first); iter != end_iter; iter++) {
-		std::cout << BIN_LETTERS[*iter];
-		if (!invalid && board.move(*iter) == MancalaMoveResult::turn_ended && board.get_ours() != max.second) {
+	for (auto const item : path_to_max) {
+		std::cout << BIN_LETTERS[item];
+		if (!invalid && board.move(item) == MancalaMoveResult::turn_ended && board.get_ours() != max) {
 			std::clog << +board.get_ours() << std::endl;
 			invalid = true;
 		}
 	}
-	std::cout << " with a total of " << +max.second << " stones." << std::endl;
+	std::cout << " with a total of " << +max << " stones." << std::endl;
 	std::cout << "This solution was found to be " << (invalid ? "INVALID" : "valid") << '.' << std::endl;
 }
 
